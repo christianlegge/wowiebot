48d84dc66a80b26e531100f42f57c7b2c9736218
scatterclegge <christianlegge888@gmail.com>
2017-02-10 18:26:19 -0330

    Fixed quotes form to make more sense

--- wowiebot/QuotesForm.Designer.cs
+++ wowiebot/QuotesForm.Designer.cs
@@ -65,9 +65,10 @@
             this.textBox1.Location = new System.Drawing.Point(23, 27);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox1.Size = new System.Drawing.Size(418, 259);
             this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
             // 
             // label1
             // 
