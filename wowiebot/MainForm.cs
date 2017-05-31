e808fae8b2d642607570b2edb4bb45860419a5e8
scatterclegge <christianlegge888@gmail.com>
2017-05-31 00:14:50 -0230

    Updated version number to v2.1.1 (release ready)

--- wowiebot/MainForm.cs
+++ wowiebot/MainForm.cs
@@ -18,7 +18,7 @@ namespace wowiebot
 
     public partial class MainForm : Form
     {
-        private const string thisVersion = "v2.1";
+        private const string thisVersion = "v2.1.1";
         private string latestVersion;
         private JObject releaseJson;
 
