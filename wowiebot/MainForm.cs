d143f9d0317e0eb2b453de520904d68cce151932
scatterclegge <christianlegge888@gmail.com>
2019-03-28 03:16:07 -0230

    upped version number

--- wowiebot/MainForm.cs
+++ wowiebot/MainForm.cs
@@ -13,7 +13,7 @@ namespace wowiebot
 
     public partial class MainForm : Form
     {
-        private const string thisVersion = "v2.3.5";
+        private const string thisVersion = "v2.3.6";
         private string latestVersion;
         private JObject releaseJson;
 
