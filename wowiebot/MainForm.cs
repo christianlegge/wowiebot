3e8a41979d22fb9ab73dcd06d41507ad6feafe7a
scatterclegge <christianlegge888@gmail.com>
2019-04-01 03:44:08 -0230

    three point oh baby

--- wowiebot/MainForm.cs
+++ wowiebot/MainForm.cs
@@ -15,7 +15,7 @@ namespace wowiebot
     {
         public static SongRequestForm songRequestForm = null;
 
-        private const string thisVersion = "v2.3.6";
+        private const string thisVersion = "v3.0";
 
         private string latestVersion;
         private JObject releaseJson;
