c49c8d936e3074065e666a65c05be424d942f0e5
scatterclegge <christianlegge888@gmail.com>
2019-04-01 03:52:21 -0230

    wrapped in massive trycatch

--- wowiebot/WowiebotApplication.cs
+++ wowiebot/WowiebotApplication.cs
@@ -9,13 +9,23 @@ namespace wowiebot
         [STAThread]
         static void Main(string[] args)
         {
-
-            
-
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("Could not load"))
+                {
+                    MessageBox.Show("Error starting application! Missing libraries? Redownload the application from GitHub to ensure you have the latest.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Unknown error! Please report this!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
